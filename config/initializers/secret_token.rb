e16ef40dd82c5ecc922add9aee08da398ea49297
Paul Gross <pgross@gmail.com>
2012-02-08 20:19:32 -0600

    Start a new rails app without active record.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibrarianRailsExample::Application.config.secret_token = 'c60cdd6add94d45ee44ee08a0d46ddf82424caa23afb272c213a5dc87e8502610f46bfbe691c9402b9070e7fc405aec62857eb5f9d7740bcbf284183a5fb2c1d'
